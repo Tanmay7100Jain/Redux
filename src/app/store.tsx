aded621625143f2474a7ecccd3ae62c98cba1250
Tanmay7100Jain <tanmayjain.24.dce@iite.indusuni.ac.in>
2025-05-04 09:38:08 +0530

    added todoslice

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from './todo/todoSlice ';
+import { todoSlice } from "./todo/todoSlice";
+
 export const store = configureStore({
-    reducer: todoReducer
-})
+  reducer: todoSlice,
+});
